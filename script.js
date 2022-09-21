a4ec551ede908b6000885f58a9c7821c6b167f0e
CrystalQuest112 <114012059+CrystalQuest112@users.noreply.github.com>
2022-09-21 16:37:27 +0200

    First Draft
    Original doc as submitted to SheCodes.

---
+++ script.js
@@ -0,0 +1,13 @@
+function subscribe() {
+        let name = prompt("What is your name?");
+        let email = prompt("What is your email?");
+        let paragraph = document.querySelector("p");
+
+        {
+          alert;
+        }
+        paragraph.innerHTML = " ✦ Thank you for subscribing " + name + "!";
+      }
+
+      button = document.querySelector("button");
+      button.addEventListener("click", subscribe);
